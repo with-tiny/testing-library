d93f3cfa680c25a5b92d838f1dc4680e67acb07d
xavikortes <javiercortesveliz@gmail.com>
2022-03-23 11:28:49 +0100

    feat: package. Added bin file

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+### Added
+
+- Bin file to test from npm run test
+
 ## [0.0.2] - 2022-03-23
 
 ### Added
---
+++ bin/t-testing.js
@@ -0,0 +1,10 @@
+import glob from 'glob'
+import '../index.js'
+
+glob(
+  '{**/__tests__/**/*.[jt]s?(x),**/?(*.)+(spec|test).[tj]s?(x)}',
+  {},
+  async (er, files) => {
+    files.forEach((file) => import(file))
+  }
+)
